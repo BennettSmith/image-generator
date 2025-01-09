df00cca64137b4a1c03b9257606c968a3bdef4e9
Bennett Smith <bennettsmith@mac.com>
2025-01-09 14:38:22 -0800

    Initial Commit

---
+++ Clean Image Generator/Clean_Image_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clean_Image_GeneratorApp.swift
+//  Clean Image Generator
+//
+//  Created by Bennett Smith on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Clean_Image_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clean Image GeneratorTests/Clean_Image_GeneratorTests.swift
@@ -0,0 +1,16 @@
+//
+//  Clean_Image_GeneratorTests.swift
+//  Clean Image GeneratorTests
+//
+//  Created by Bennett Smith on 1/9/25.
+//
+
+import Testing
+
+struct Clean_Image_GeneratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
