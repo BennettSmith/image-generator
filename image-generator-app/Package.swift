5e5456db90239f01da638563bfe24eff1b3f82a7
Bennett Smith <bennettsmith@mac.com>
2025-01-14 15:06:52 -0800

    chore: remove unnecessary comments

--- image-generator-app/Package.swift
+++ image-generator-app/Package.swift
@@ -7,7 +7,6 @@ let package = Package(
     name: "image-generator-app",
     platforms: [.macOS(.v12), .iOS(.v15)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ImageGeneratorUseCases",
             targets: ["ImageGeneratorUseCases"]),
@@ -19,8 +18,6 @@ let package = Package(
         .package(path: "../image-generator-core"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ImageGeneratorUseCases"
         ),
